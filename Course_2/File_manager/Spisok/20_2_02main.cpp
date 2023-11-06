0ce381674e5bc52f49d1eed4da2c6d06c2c28932
Alex <nikitapizdarenko@gmail.com>
2023-11-06 11:55:22 +0500

    modified:   Course_2/File_manager/Spisok/20_2_02main.cpp 	modified:   Course_2/File_manager/Spisok/20_2_02task.cpp 	modified:   Course_2/File_manager/Spisok/Test/test 	modified:   Course_2/File_manager/Spisok/Test/test.cpp 	modified:   Course_2/File_manager/Spisok/makefile

--- Course_2/File_manager/Spisok/20_2_02main.cpp
+++ Course_2/File_manager/Spisok/20_2_02main.cpp
@@ -8,17 +8,17 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    char *dir_now = strdup("");
     file_manager_t fm;
     setup_file_manager(&fm);
     fm.create(100);
-    fm.create_dir("/Main");
+    fm.create_dir("Main");
     fm.create_dir("/Main/test1");
     // fm.destroy();
-    // fm.create_dir();
     // fm.create_file();
     // fm.remove();
     // fm.change_dir();
-    // fm.get_cur_dir();
+    fm.get_cur_dir(dir_now);
     // fm.list();
     return 0;
 }
--- Course_2/File_manager/Spisok/20_2_02task.cpp
+++ Course_2/File_manager/Spisok/20_2_02task.cpp
@@ -26,12 +26,13 @@ int ptr_create_dir(const char *path);
 // int ptr_create_file(const char *path, int file_size);
 // int ptr_remove(const char *path, int recursive);
 // int ptr_change_dir(const char *path);
-// void ptr_get_cur_dir(char *dst);
+void ptr_get_cur_dir(char *dst);
 // int ptr_list(const char *path, int dir_first);
 //=================================================================
 
 //=================[ Прототипы дополнительных функций ]=================
-// int CheckDir();
+int CheckDir();
+void C_DoubleList();
 //======================================================================
 
 //=================[  Работа с указателями на структуры ]=================
@@ -43,7 +44,7 @@ void setup_file_manager(file_manager_t *fm)
     // fm->create_file = ptr_create_file;
     // fm->remove = ptr_remove;
     // fm->change_dir = ptr_change_dir;
-    // fm->get_cur_dir = ptr_get_cur_dir;
+    fm->get_cur_dir = ptr_get_cur_dir;
     // fm->list = ptr_list;
 }
 //=========================================================================
@@ -58,26 +59,40 @@ int ptr_create(int disk_size)
             node->size = disk_size;
             node->absolute_path = strdup("/");
             node->cur_dir = strdup(node->absolute_path);
-            fprintf(stdout, "%s\n", node->cur_dir);
-            fprintf(stdout, "%d\n", node->size);
             return 1;
         }
         else
             fprintf(stdout, "Error with malloc for fm\n");
     }
     else
-    {
         fprintf(stdout, "Error: Incorrect size\n");
-        return 0;
-    }
-    return 2;
+    return 0;
 }
 
 int ptr_create_dir(const char *path)
 {
-    node->new_dir = strdup(path);
-    node->new_dir = strdup(strcat(node->new_dir, "/"));
-    node->cur_dir = strdup(strcat(node->cur_dir, node->new_dir));
-    fprintf(stdout, "%s\n", node->cur_dir);
+    if (strcmp(node->cur_dir, node->absolute_path) == 0)
+    {
+       node->new_dir = strdup(path);
+       
+    }
+
+    // node->new_dir = strdup(path);
+    // node->new_dir = strdup(strcat(node->new_dir, "/"));
+    // node->cur_dir = strdup(strcat(node->cur_dir, node->new_dir));
     return 1;
+}
+
+void ptr_get_cur_dir(char *dst)
+{
+    dst = strdup(node->cur_dir);
+    fprintf(stdout, "%s\n", dst);
+}
+
+int CheckDir()
+{
+}
+
+void C_DoubleList()
+{
 }
